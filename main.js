ef78bb36777697887729fb5fe2bbde881cd046c8
Nakayama <123441983+Shohei-Nakayama@users.noreply.github.com>
2024-07-04 13:22:14 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const myPrice = 100;
-
-if (myPrice >=50){
-    alert('myPriceは５０以上です');
-}else if(myPrice >=10){
-    alert('myPriceは10以上５０未満です');
-}else{
-    alert('myPriceは１０未満です');
-}
-
+var app = new Vue({
+el:'#app',
+data:{
+    myPrice:""
+},
+computed:{
+        function(myPrice){
+                                if (myPrice >=50){
+                                    alert('myPriceは５０以上です')
+                                }else if(myPrice >=10){
+                                    alert('myPriceは10以上５０未満です')
+                                }else{
+                                    alert('myPriceは１０未満です')
+                                  }
+                                }
+                        }
+})
